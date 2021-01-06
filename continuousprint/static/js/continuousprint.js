63615eddf3aa388246c186301a9028748a5f7085
Zinc-OS <63823426+Zinc-OS@users.noreply.github.com>
2021-01-06 18:38:42 -0500

    Update continuousprint.js

--- continuousprint/static/js/continuousprint.js
+++ continuousprint/static/js/continuousprint.js
@@ -194,8 +194,8 @@ $(function() {
                 var temp2=$("#queue_list").children(".n"+(data-1)).children(".queue-row-container").children(".queue-inner-row-container").children(".file-name").text();
                 $("#queue_list").children(".n"+data).children(".queue-row-container").children(".queue-inner-row-container").children(".file-name").text(temp2);
                 $("#queue_list").children(".n"+(data-1)).children(".queue-row-container").children(".queue-inner-row-container").children(".file-name").text(temp);
-                $("#queue_list").children(".n"+data).children(".queue-row-container").children(".queue-inner-row-container").children(".file-name").attr("value",temp4);
-                $("#queue_list").children(".n"+data-1).children(".queue-row-container").children(".queue-inner-row-container").children(".file-name").attr("value",temp3);
+                $("#queue_list").children(".n"+data).children(".queue-row-container").children(".queue-inner-row-container").children(".file-name").attr("value",parseInt(temp4));
+                $("#queue_list").children(".n"+data-1).children(".queue-row-container").children(".queue-inner-row-container").children(".file-name").attr("value",parseInt(temp3));
   
             }
             if(CMD=="DOWN"){
@@ -205,8 +205,8 @@ $(function() {
                 var temp2=$("#queue_list").children(".n"+(data+1)).children(".queue-row-container").children(".queue-inner-row-container").children(".file-name").text();
                 $("#queue_list").children(".n"+data).children(".queue-row-container").children(".queue-inner-row-container").children(".file-name").text(temp2);
                 $("#queue_list").children(".n"+(data+1)).children(".queue-row-container").children(".queue-inner-row-container").children(".file-name").text(temp); 
-                $("#queue_list").children(".n"+data).children(".queue-row-container").children(".queue-inner-row-container").children(".file-name").attr("value",temp4);
-                $("#queue_list").children(".n"+data+1).children(".queue-row-container").children(".queue-inner-row-container").children(".file-name").attr("value",temp3);
+                $("#queue_list").children(".n"+data).children(".queue-row-container").children(".queue-inner-row-container").children(".file-name").attr("value",parseInt(temp4));
+                $("#queue_list").children(".n"+data+1).children(".queue-row-container").children(".queue-inner-row-container").children(".file-name").attr("value",parseInt(temp3));
             }
              
              
