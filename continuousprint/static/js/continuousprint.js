951bf3458d31c4dbddb1645f0c177a72ca342f15
Zinc-OS <63823426+Zinc-OS@users.noreply.github.com>
2021-01-18 19:32:59 -0500

    Update continuousprint.js

--- continuousprint/static/js/continuousprint.js
+++ continuousprint/static/js/continuousprint.js
@@ -462,9 +462,12 @@ $(function() {
     */
     $(document).ready(function(){
 			let regex = /<div class="btn-group action-buttons">([\s\S]*)<.div>/mi;
-			let template = '<div class="btn btn-mini" data-bind="click: function() { if ($root.loginState.isUser()) { $root.addtoqueue($data) } else { return; } }, visible: true title="Show Thumbnail" style="display: none;"><i class="Q">Q</i></div>';//Q for queue
-			$("#files_template_machinecode").text(function () {
-				var return_value =  $(this).text();
+			let template = '<div class="btn btn-mini" data-bind="click: function() { if ($root.loginState.isUser()) { $root.addtoqueue($data) } else { return; } }, visible: (true) title="Show Thumbnail" style="display: none;"><i class="Q">Q</i></div>';//Q for queue
+            let inline_thumbnail_template = '<div class="row-fluid inline-inline-dumbo-test-an-w-ttp-the-ttp-project" ' +
+                                '>' +
+                                '</div>'
+            $("#files_template_machinecode").text(function () {
+				var return_value = inline_thumbnail_template +  $(this).text();
 				return_value = return_value.replace(regex, '<div class="btn-group action-buttons">$1	' + template + '></div>');
 				return return_value
 			});
