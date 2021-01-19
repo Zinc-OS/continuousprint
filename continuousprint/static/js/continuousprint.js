ca699515a34bf3f0147a7ccfa1573164b4326ae8
Zinc-OS <63823426+Zinc-OS@users.noreply.github.com>
2021-01-18 21:43:32 -0500

    Update continuousprint.js

--- continuousprint/static/js/continuousprint.js
+++ continuousprint/static/js/continuousprint.js
@@ -481,7 +481,7 @@ $(function() {
 
 			$("#files_template_machinecode").text(function () {
 				var return_value=$(this).text();
-				return_value = return_value.replace(regex, '<div class="btn-group action-buttons">$1	' + template + '></div>');
+				return_value = return_value+template;
 				return return_value
 			});
 		});
